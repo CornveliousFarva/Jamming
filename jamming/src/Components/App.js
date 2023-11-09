e4c92ab408069239bf4bb5ea69f4de37e8a864c2
Greg Schuman <gcschuman@hotmail.com>
2023-11-09 12:44:33 -0800

    Added imports in app.js, added a Spotify util, and refactored the index css.

---
+++ jamming/src/Components/App.js
@@ -0,0 +1,6 @@
+import { useState, useCallback } from "react";
+
+import Playlist from "../Components/Playlist";
+import SearchBar from "../Components/SearchBar";
+import SearchResults from "../Components/SearchResults";
+import Spotify from "../Utils/Spotify";
