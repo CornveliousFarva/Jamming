9dfe54e94ae085671e1d20850d1e2d99b1e8ee0d
Greg Schuman <gcschuman@hotmail.com>
2023-11-14 13:28:13 -0800

    Added playlist script.

---
+++ jamming/src/Components/Playlist.js
@@ -0,0 +1,29 @@
+import { useCallback } from "react";
+
+import "../App.css";
+
+import TrackList from "../Components/Tracklist";
+
+export default function Playlist(){
+    const Playlist = (props) => {
+        const handleNameChange = useCallback(
+          (event) => {
+            props.onNameChange(event.target.value);
+          },
+          [props.onNameChange]
+        );
+        return (
+            <div className="Playlist">
+              <input onChange={handleNameChange} defaultValue={"New Playlist"} />
+              <TrackList
+                tracks={props.playlistTracks}
+                isRemoval={true}
+                onRemove={props.onRemove}
+              />
+              <button className="Playlist-save" onClick={props.onSave}>
+                SAVE TO SPOTIFY
+              </button>
+            </div>
+          );
+        };
+}
