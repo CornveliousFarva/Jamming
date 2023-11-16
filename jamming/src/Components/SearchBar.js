6012d48d3b50963ebbd4cd2b959f21a57b07edfe
Greg Schuman <gcschuman@hotmail.com>
2023-11-16 13:08:41 -0800

    Added the script for search bar.

---
+++ jamming/src/Components/SearchBar.js
@@ -0,0 +1,24 @@
+import React, { useState, useCallback, props } from "react";
+
+import "jamming/src/App.css"
+
+export default function SearchBar(){
+    const [term, setTerm] = useState("");
+
+  const handleTermChange = useCallback((event) => {
+    setTerm(event.target.value);
+  }, []);
+
+  const search = useCallback(() => {
+    props.onSearch(term);
+  }, [props.onSearch, term]);
+
+  return (
+    <div className="SearchBar">
+      <input placeholder="Enter A Song Title" onChange={handleTermChange} />
+      <button className="SearchButton" onClick={search}>
+        SEARCH
+      </button>
+    </div>
+  ); 
+}
