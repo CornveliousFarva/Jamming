f15f4aa942f246db567ecc1e0eca57f2f1a4c9d8
Greg Schuman <gcschuman@hotmail.com>
2023-11-17 09:21:28 -0800

    Refactored the Playlist script and moved the Playlist CSS into its own file.

--- jamming/src/Components/Playlist/Playlist.js
+++ jamming/src/Components/Playlist/Playlist.js
@@ -1,29 +1,30 @@
-import { useCallback } from "react";
+import React, { useCallback } from "react";
 
-import "../App.css";
+import "./Playlist.css";
 
-import TrackList from "../Tracklist/Tracklist";
+import TrackList from "../TrackList/TrackList";
 
-export default function Playlist(){
-    const Playlist = (props) => {
-        const handleNameChange = useCallback(
-          (event) => {
-            props.onNameChange(event.target.value);
-          },
-          [props.onNameChange]
-        );
-        return (
-            <div className="Playlist">
-              <input onChange={handleNameChange} defaultValue={"New Playlist"} />
-              <TrackList
-                tracks={props.playlistTracks}
-                isRemoval={true}
-                onRemove={props.onRemove}
-              />
-              <button className="Playlist-save" onClick={props.onSave}>
-                SAVE TO SPOTIFY
-              </button>
-            </div>
-          );
-        };
-}
+const Playlist = (props) => {
+  const handleNameChange = useCallback(
+    (event) => {
+      props.onNameChange(event.target.value);
+    },
+    [props.onNameChange]
+  );
+
+  return (
+    <div className="Playlist">
+      <input onChange={handleNameChange} defaultValue={"New Playlist"} />
+      <TrackList
+        tracks={props.playlistTracks}
+        isRemoval={true}
+        onRemove={props.onRemove}
+      />
+      <button className="Playlist-save" onClick={props.onSave}>
+        SAVE TO SPOTIFY
+      </button>
+    </div>
+  );
+};
+
+export default Playlist;
